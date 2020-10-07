9cd6c0de3195efe5614f5b363f5694bdc8ee9aff
Huy Dat <dat.nguyen.d16ptit@gmail.com>
2020-10-08 00:18:51 +0700

    user login logout

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,8 @@ Route::group(['middleware' => 'localization'], function() {
         Route::name('admin.')->group(function() {
             Route::get('login', 'Admin\LoginController@getLogin')->name('getLogin');
             Route::post('login', 'Admin\LoginController@postLogin')->name('postLogin');
+            Route::get('logout', 'Admin\LoginController@logout')->name('logout');
             Route::group(['middleware' => 'checkAdminLogin'], function() {
-                Route::get('logout', 'Admin\LoginController@logout')->name('logout');
                 Route::get('change_password', 'Admin\ChangePasswordController@getChangePassword')->name('getChangePassword');
                 Route::post('change_password', 'Admin\ChangePasswordController@postChangePassword')->name('postChangePassword');
                 Route::get('/', 'Admin\HomeController@dashboard')->name('dashboard');
@@ -31,8 +31,14 @@ Route::group(['middleware' => 'localization'], function() {
         });
     });
 
-    Route::name('user.')->group(function() {
-        Route::get('/', 'HomeController@home')->name('home');
-        Route::get('product', 'ProductController@index')->name('product');
+    Route::get('login', 'LoginController@getLogin')->name('getLogin');
+    Route::post('login', 'LoginController@postLogin')->name('postLogin');
+    Route::get('logout', 'LoginController@logout')->name('logout');
+    Route::get('/', 'HomeController@home')->name('home');
+    Route::get('product', 'ProductController@index')->name('product');
+
+    Route::group(['middleware' => 'checkUserLogin'], function() {
+
     });
+
 });

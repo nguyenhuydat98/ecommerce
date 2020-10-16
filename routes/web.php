e9ea6eefc6ea5000cc8bd0e6dc6161defcc4f3de
Huy Dat <dat.nguyen.d16ptit@gmail.com>
2020-10-16 23:13:34 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-
 Route::group(['middleware' => 'localization'], function() {
     Route::get('lang/{language}', 'LocalizationController@changeLanguage')->name('localization');
 
@@ -42,5 +40,4 @@ Route::group(['middleware' => 'localization'], function() {
     Route::group(['middleware' => 'checkUserLogin'], function() {
 
     });
-
 });
